c2ff2bf41e5e077228c52369025dfaaf9ff49b7b
James Buncle <jbuncle@hotmail.com>
2017-11-21 23:41:45 +0000

    Added get method to HttpClient

--- src/main/java/uk/co/jbuncle/hnapclient/http/HttpClient.java
+++ src/main/java/uk/co/jbuncle/hnapclient/http/HttpClient.java
@@ -9,9 +9,12 @@ import java.util.Map;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpResponseException;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 
 /**
  *
@@ -34,4 +37,49 @@ public class HttpClient {
         }
         return IOUtils.toString(response.getEntity().getContent());
     }
+
+    public String get(final String url, final Map<String, String> headers)
+            throws UnsupportedEncodingException, IOException, UnsupportedOperationException {
+        final HttpGet get = new HttpGet(url);
+
+        return performRequest(headers, get);
+    }
+
+    private String performRequest(
+            final Map<String, String> headers,
+            final HttpRequestBase httpRequestBase
+    ) throws IOException, UnsupportedOperationException, HttpResponseException {
+        addHeaders(headers, httpRequestBase);
+        final HttpResponse httpResponse = getResponse(httpRequestBase);
+        checkResponseCode(httpResponse);
+        return getResponseBody(httpResponse);
+    }
+
+    private void setRequestBody(final String body, final HttpPost httpPost) throws UnsupportedEncodingException {
+        final StringEntity entity = new StringEntity(body);
+        httpPost.setEntity(entity);
+    }
+
+    private static String getResponseBody(final HttpResponse response) throws UnsupportedOperationException, IOException {
+        return IOUtils.toString(response.getEntity().getContent());
+    }
+
+    private void checkResponseCode(final HttpResponse httpResponse) throws HttpResponseException {
+        if (httpResponse.getStatusLine().getStatusCode() != 200) {
+            throw new HttpResponseException(httpResponse.getStatusLine().getStatusCode(), httpResponse.getStatusLine().getReasonPhrase());
+        }
+    }
+
+    private HttpResponse getResponse(final HttpRequestBase httpRequestBase) throws IOException {
+        org.apache.http.client.HttpClient client = HttpClientBuilder.create().build();
+        final HttpResponse response = client.execute(httpRequestBase);
+        return response;
+    }
+
+    private void addHeaders(final Map<String, String> headers, final HttpRequestBase httpRequestBase) {
+        headers.entrySet().forEach((Map.Entry<String, String> header) -> {
+            httpRequestBase.addHeader(header.getKey(), header.getValue());
+        });
+    }
+
 }
